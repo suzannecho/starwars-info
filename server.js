a388b9b0e565564e2e591312bf3603b7bfbfce1d
Suzanne Cho <suzcho@uw.edu>
2019-08-14 19:11:32 -0700

    added js file for starwars

---
+++ server.js
@@ -0,0 +1,52 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+// Data
+// ===========================================================
+var yoda = {
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000
+};
+
+var darthmaul = {
+  name: "Darth Maul",
+  role: "Sith Lord",
+  age: 200,
+  forcePoints: 1200
+};
+
+var Obi_wan_Kenobi = {
+  name: "Obi Wan Kenobi"
+  role: "Jedi",
+  age: 100,
+  forcePoints: 1500
+};
+
+// Routes
+// ===========================================================
+app.get("/", function(req, res) {
+  res.send("Welcome to the Star Wars Page!");
+});
+
+app.get("/yoda", function(req, res) {
+  res.json(yoda);
+});
+
+app.get("/darthmaul", function(req, res) {
+  res.json(darthmaul);
+});
+
+app.get("/obiwankenobi", function(req, res) {
+  res.send(Obi_wan_kenobi)
+})
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
